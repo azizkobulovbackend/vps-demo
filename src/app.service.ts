9198d929ab7440d95e1f8b49b1bf32c02241410b
a.kobulov <a.kobulov@agrobank.uz>
2026-08-12 11:44:00 +0500

    fix: correct greeting case in getHello method

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,7 @@ export class AppService {
   constructor(private readonly prisma: PrismaService) {}
 
   getHello(): string {
-    return 'Hello world!';
+    return 'Hello WORLD!';
   }
 
   async getUser() {
