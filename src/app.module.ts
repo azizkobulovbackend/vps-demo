a95c3dc7fa375474d2e7a659e51970d8ef228e7b
a.kobulov <a.kobulov@agrobank.uz>
2026-08-15 23:34:35 +0500

    feat: add Loki logging support with nestjs-pino integration
    - Updated docker-compose.yml to include Loki service for log aggregation.
    - Added new dependencies for logging: @nestjs/config, nestjs-pino, pino, pino-http, and pino-loki.
    - Created logger.config.ts to configure pino with Loki transport.
    - Modified app.module.ts to import LoggerModule and use async configuration.
    - Updated main.ts to set up the logger for the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { MetricsModule } from './metrics/metrics.module';
+import { LoggerModule } from 'nestjs-pino';
+import { pinoConfig } from './logger.config';
 
 @Module({
-  imports: [PrismaModule, MetricsModule],
+  imports: [PrismaModule, MetricsModule, LoggerModule.forRootAsync(pinoConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
